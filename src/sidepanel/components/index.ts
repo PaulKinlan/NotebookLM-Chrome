b11ffe50a25c0040e88113c0902da3bfc9774d89
Joseph Mearman <joseph@mearman.co.uk>
2026-01-12 20:59:56 +0000

    chore(components): update component barrel exports
    Remove mount* function exports (no longer needed with stateful
    components). Add new stateful component exports:
    - SourcesList
    - ChatMessages
    - ChatInput
    - PickerModal
    - ChatTabStateful
    - SlashCommand type
    - PickerModalProps type

---
+++ src/sidepanel/components/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Components Barrel Export
+ *
+ * Exports all React-like components for the sidepanel.
+ */
+
+export { SourcesList } from './SourcesList.tsx'
+export { ChatMessages } from './ChatMessages.tsx'
+export { ChatInput } from './ChatInput.tsx'
+export { PickerModal } from './PickerModal.tsx'
+export { ChatTabStateful } from './ChatTabStateful.tsx'
+export type { SlashCommand } from './ChatInput.tsx'
+export type { PickerModalProps } from './PickerModal.tsx'
